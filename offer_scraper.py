6e813f96273c4f11f0152e79d3e97ec6302d14f9
Mariana Oleinic <moleinic@griddynamics.com>
2024-05-27 14:25:43 +0300

    refactor: make Check Offers logic async

--- offer_scraper.py
+++ offer_scraper.py
@@ -1,20 +1,16 @@
-import requests
+import aiohttp
 from bs4 import BeautifulSoup
-import sqlite3
 
-connection = sqlite3.connect('vacation_hot_offers.db')
-cursor = connection.cursor()
 
-cursor.execute('''DROP TABLE IF EXISTS offers''')
+async def fetch(session, url):
+    async with session.get(url, ssl=False) as response:
+        if response.status == 200:
+            return await response.text()
+        else:
+            raise ValueError(f"Failed to fetch data from {url}. Status code: {response.status}")
 
-cursor.execute('''CREATE TABLE offers (
-               agency_name TEXT,
-               offer_title TEXT,
-               offer_date TEXT,
-               offer_price TEXT
-)''')
 
-def scrape_offers_and_populate_db(agency):
+async def scrape_offers(agency):
     agencies_data = {
         'Admiral TUR': {
             'url': 'https://admiral.travel/en',
@@ -34,27 +30,26 @@ def scrape_offers_and_populate_db(agency):
         }
     }
 
-    if agency in agencies_data:
-        url = agencies_data[agency]['url']
-        response = requests.get(url)
-        if response.status_code == 200:
-            result = response.text
-            soup = BeautifulSoup(result, 'html.parser')
-            selectors = agencies_data[agency]['selectors']
-            
-            titles_data = soup.findAll(class_=selectors['offers'])
-            offers_titles = [div.get_text(strip=True) for div in titles_data]
-            
-            dates_data = soup.findAll(class_=selectors['dates'])
-            offers_dates = [div.get_text(strip=True) for div in dates_data]
-            
-            prices_data = soup.findAll(class_=selectors['prices'])
-            offers_prices = [div.get_text(strip=True) for div in prices_data]
+    if agency not in agencies_data:
+        raise ValueError(f"Agency '{agency}' is not supported.")
+    
+    url = agencies_data[agency]['url']
+    
+    async with aiohttp.ClientSession() as session:
+        result = await fetch(session, url)
+    
+    soup = BeautifulSoup(result, 'html.parser')
+    selectors = agencies_data[agency]['selectors']
+    
+    titles_data = soup.findAll(class_=selectors['offers'])
+    offers_titles = [div.get_text(strip=True) for div in titles_data]
+    
+    dates_data = soup.findAll(class_=selectors['dates'])
+    offers_dates = [div.get_text(strip=True) for div in dates_data]
+    
+    prices_data = soup.findAll(class_=selectors['prices'])
+    offers_prices = [div.get_text(strip=True) for div in prices_data]
+    
+    agency_name = agency 
 
-            agency_name = agency
-
-            for title, date, price in zip(offers_titles, offers_dates, offers_prices): 
-                cursor.execute('''INSERT INTO offers (agency_name, offer_title, offer_date, offer_price) VALUES (?, ?, ?, ?)''', (agency_name, title, date, price))
-
-        connection.commit()
-
+    return agency_name, offers_titles, offers_dates, offers_prices
