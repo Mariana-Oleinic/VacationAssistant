d2b5ed90560b2b4cf768f99231570eb04447ad35
Mariana Oleinic <moleinic@griddynamics.com>
2024-05-27 14:27:59 +0300

    rearrange code in vacation_assistant.py as per latest refactor

--- vacation_assistant.py
+++ vacation_assistant.py
@@ -4,13 +4,12 @@ Vacation Planner Assistant
 import tkinter as tk
 from tkinter import font
 from tkinter import ttk
-import sqlite3
 from datetime import datetime
-from io import BytesIO
 from PIL import Image, ImageTk
-import requests
-from offer_scraper import scrape_offers_and_populate_db
+from database import query_database
 from convertor_scraper import scrape_currency_conversion
+from weather_fetcher import fetch_weather_forecast, fetch_weather_icon
+
 
 root = tk.Tk()
 root.title("Vacation Planner Assistant")
@@ -54,33 +53,21 @@ separator1.grid(row=0, column=1, sticky='ns', padx=5, pady=5)
 separator2 = ttk.Separator(main_frame, orient='vertical')
 separator2.grid(row=0, column=3, sticky='ns', padx=5, pady=5)
 
-# Code for "check offers" feature:
-def query_db():
-    """Query the database for offers"""
-    agency = agency_var.get()
-    scrape_offers_and_populate_db(agency)
-    connection = sqlite3.connect('vacation_hot_offers.db')
-    cursor = connection.cursor()
-    cursor.execute(
-        "SELECT offer_title, offer_date, offer_price "
-        "FROM offers "
-        "WHERE agency_name = ?",
-        (agency,)
-    )
-    records = cursor.fetchmany(6)
-    connection.close()
-    return records
 
+# Code for "check offers" feature:
 def on_offers_check():
     """When clicking on "Check Offers" button, this function updates the offers_result_label"""
-    records = query_db()
+    agency = agency_var.get()
+    records = query_database(agency)
     result = "List of Hot Offers 🔥:\n\n"
     result += "\n\n".join(" ".join(map(str, record)) for record in records)
     offers_result_label.config(text=result)
 
+
 def reset_offers():
     """Clear the offers result label"""
     offers_result_label.config(text="")
+
 
 offers_label = tk.Label(left_frame, text="Check vacation Hot Offers 🏝️:", font=bold_font)
 offers_label.pack(pady=5)
@@ -111,6 +98,7 @@ tk.Button(
 offers_result_label = tk.Label(left_frame, text="")
 offers_result_label.pack(padx=20, pady=10)
 
+
 # Code for "check budget" feature:
 def on_budget_check():
     """When clicking on "Check Budget" button, this function updates the budget_result_label"""
@@ -120,10 +108,12 @@ def on_budget_check():
     result = scrape_currency_conversion(amount, from_currency, to_currency)
     budget_result_label.config(text='Result: ' + result, font=bold_font)
 
+
 def reset_budget():
     """Clear the budget result label"""
     budget_result_label.config(text="")
     amount_entry.delete(0, tk.END)
+
 
 budget_label = tk.Label(center_frame, text="Check budget 💰:", font=bold_font)
 budget_label.pack(pady=5)
@@ -168,55 +158,8 @@ tk.Button(
 budget_result_label = tk.Label(center_frame, text="")
 budget_result_label.pack(padx=20, pady=10)
 
+
 # Code for "check weather" feature:
-def fetch_weather_forecast(city, api_key, current_weather_url, forecast_url):
-    """
-    Fetches the current weather and daily forecasts for a given city.
-    Args:
-    - city (str): The name of the city.
-    - api_key (str): The API key for accessing the weather API.
-    - current_weather_url (str): The URL template for fetching current weather data.
-    - forecast_url (str): The URL template for fetching forecast data.
-    Returns:
-    - Tuple[dict, list]: A tuple containing the current weather data and daily forecasts.
-                         Returns (None, None) if the request fails.
-    """
-    current_weather_response = requests.get(current_weather_url.format(city, api_key))
-    if current_weather_response.status_code == 200:
-        data = current_weather_response.json()
-        lat, lon = data['coord']['lat'], data['coord']['lon']
-        weather_data = {
-            "city": data["name"],
-            "temperature": round(data["main"]["temp"] - 273.15),
-            "description": data["weather"][0]["description"],
-            "icon": data["weather"][0]["icon"],
-            'day': datetime.fromtimestamp(data['dt']).strftime('%A')
-        }
-        forecast_response = requests.get(forecast_url.format(lat, lon, api_key))
-        if forecast_response.status_code == 200:
-            data2 = forecast_response.json()
-            daily_forecasts = extract_daily_forecasts(data2)
-            return weather_data, daily_forecasts
-    else:
-        return None, None
-  
-
-def extract_daily_forecasts(response):
-    """
-    Extracts daily forecasts from the API response.
-    Args:
-   - response (dict): The API response containing forecast data for multiple days and at different hours.
-    Returns:
-    - list: A list of daily forecasts at specified time 12:00:00.
-    """
-    daily_forecasts = {}
-    for forecast in response['list']:
-        date, time = forecast['dt_txt'].split(" ")
-        if date != datetime.now() not in daily_forecasts or time == "12:00:00":
-            daily_forecasts[date] = forecast
-
-    return list(daily_forecasts.values())
-
 def on_weather_check():
     """When clicking on "Check Weather" button, this function updates the weather_result_label"""
     city = city_entry.get()
@@ -251,15 +194,9 @@ def display_weather_forecast(weather_data, daily_forecasts):
     description_label = tk.Label(weather_frame, text=weather_data['description'], font=("Helvetica", 12))
     description_label.pack()
 
-    # Fetch the weather icon
-    icon_url = f"http://openweathermap.org/img/w/{weather_data['icon']}.png"
-    response = requests.get(icon_url)
-    image_data = response.content
-    image = Image.open(BytesIO(image_data))
-    photo = ImageTk.PhotoImage(image)
-
-    icon_label = tk.Label(weather_frame, image=photo)
-    icon_label.image = photo
+    current_weather_icon = fetch_weather_icon(weather_data['icon'])
+    icon_label = tk.Label(weather_frame, image=current_weather_icon)
+    icon_label.image = current_weather_icon
     icon_label.pack()
 
     for forecast in daily_forecasts:
@@ -272,15 +209,11 @@ def display_weather_forecast(weather_data, daily_forecasts):
         forecast_description_label = tk.Label(weather_frame, text=forecast['weather'][0]['description'], font=("Helvetica", 12))
         forecast_description_label.pack()
 
-        forecast_icon_url = f"http://openweathermap.org/img/w/{forecast['weather'][0]['icon']}.png"
-        forecast_response = requests.get(forecast_icon_url)
-        forecast_image_data = forecast_response.content
-        forecast_image = Image.open(BytesIO(forecast_image_data))
-        forecast_photo = ImageTk.PhotoImage(forecast_image)
-
-        forecast_icon_label = tk.Label(weather_frame, image=forecast_photo)
-        forecast_icon_label.image = forecast_photo
+        forecast_icon = fetch_weather_icon(forecast['weather'][0]['icon'])
+        forecast_icon_label = tk.Label(weather_frame, image=forecast_icon)
+        forecast_icon_label.image = forecast_icon
         forecast_icon_label.pack()
+
 
 def reset_weather():
     """Clear the weather result label, clear the city field, and destroy weather frame"""
@@ -288,6 +221,7 @@ def reset_weather():
     weather_result_label.config(text="")
     if 'weather_frame' in globals():
         weather_frame.destroy()
+
 
 weather_label = tk.Label(right_frame, text="Check weather forecast ⛅️:", font=bold_font)
 weather_label.pack(pady=5)
