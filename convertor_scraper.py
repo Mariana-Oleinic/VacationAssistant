797354e2c876ac67d85512065278645126caa369
Mariana Oleinic <moleinic@griddynamics.com>
2024-05-28 20:01:48 +0300

    adjust script to use page.eval_on_selector instead of page.evaluate

--- convertor_scraper.py
+++ convertor_scraper.py
@@ -6,20 +6,19 @@ def scrape_currency_conversion(amount, from_currency, to_currency):
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=True)
             page = browser.new_page()
-            page.goto('https://www.curs.md/en') 
-			
+            page.goto('https://www.curs.md/en')
+
             amount_selector = 'input[placeholder="Input sum"]'
             page.fill(amount_selector, amount)
-			
+
             from_currency_selector = f'.btn.src-currency:has-text("{from_currency}")'
             page.click(from_currency_selector, force=True)
-			
+
             to_currency_selector = f'.btn.tgt-currency:has-text("{to_currency}")'
             page.click(to_currency_selector, force=True)
 
-            converted_amount = page.evaluate('document.querySelector(".form-control.tgt-sum").value')
+            converted_amount = page.eval_on_selector('.tgt-sum', 'el => el.value')
             return f'{converted_amount} {to_currency}'
     except Exception as e:
         print("An error occurred", e)
         return None
-		
