db6a2bc1f6544cfd2c789c43e873b566f8f2198a
Mariana Oleinic <moleinic@griddynamics.com>
2024-05-28 20:02:39 +0300

    adjust test to reflect the convertor_scraper script changes

--- tests/test_convertor_scraper.py
+++ tests/test_convertor_scraper.py
@@ -11,17 +11,12 @@ def mock_playwright():
     with patch('convertor_scraper.sync_playwright') as mock_sync_playwright:
         yield mock_sync_playwright
 
-@pytest.fixture
-def mock_paste():
-    with patch('convertor_scraper.pyperclip.paste') as mock_paste:
-        yield mock_paste
-
-def test_scrape_currency_conversion_success(mock_playwright, mock_paste):
+def test_scrape_currency_conversion_success(mock_playwright):
     mock_browser = MagicMock()
     mock_page = MagicMock()
     mock_playwright.return_value.__enter__.return_value.chromium.launch.return_value = mock_browser
     mock_browser.new_page.return_value = mock_page
-    mock_paste.return_value = '100'
+    mock_page.eval_on_selector.return_value = '100'
 
     result = scrape_currency_conversion('50', 'USD', 'EUR')
 
@@ -29,7 +24,7 @@ def test_scrape_currency_conversion_success(mock_playwright, mock_paste):
     mock_page.fill.assert_called_once_with('input[placeholder="Input sum"]', '50')
     mock_page.click.assert_any_call('.btn.src-currency:has-text("USD")', force=True)
     mock_page.click.assert_any_call('.btn.tgt-currency:has-text("EUR")', force=True)
-    mock_page.click.assert_any_call('button.copy-result-btn.btn')
+    mock_page.eval_on_selector.assert_called_once_with('.tgt-sum', 'el => el.value')
     assert result == '100 EUR'
 
 def test_scrape_currency_conversion_failure(mock_playwright):
